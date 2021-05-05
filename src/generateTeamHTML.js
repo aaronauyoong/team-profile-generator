06af6a88c8b17f4728b692321444ee9d7bb484ca
Aaron Au Yoong <auyoong.aaron@gmail.com>
2021-05-05 23:58:51 +0800

    wrote pseudo code.

---
+++ src/generateTeamHTML.js
@@ -0,0 +1,15 @@
+// Function to generate manager card
+
+// Function to generate engineer card
+
+// Function to generate intern card
+
+// Function to push employee cards to page
+
+// Template literals for team profile HTML
+
+// Export module
+module.exports = {
+    generateTeamHTML
+}; 
+
